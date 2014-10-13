df58481d163a25620955ee47c5178f56f8d859d0
GemmaLou <30063@longroad.ac.uk>
2014-10-13 21:20:30 +0100

    pre

---
+++ iteration pre 1.py
@@ -0,0 +1,5 @@
+message=input("Please enter a message to be displayed: ")
+number=int(input("How many times do you want this message repeated?: "))
+for count in range(number):
+    print(message)
+    
---
+++ iteration pre 2.py
@@ -0,0 +1,4 @@
+password=input("Please enter the password: ")
+while password != "secret":
+    password=input("Incorrect. Try again: ")
+print("You now have access to the system.")
